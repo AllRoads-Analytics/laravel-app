6e26a4157de178814f02ba42099a7752789fc982
Tim <tim.cieplowski@gmail.com>
2022-01-05 16:51:16 -0500

    rename Cashier migrations classes

--- database/migrations/2021_12_29_000003_create_subscription_items_table.php
+++ database/migrations/2021_12_29_000003_create_subscription_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSubscriptionItemsTable extends Migration
+class CreateSubscriptionItemsTableCashier extends Migration
 {
     /**
      * Run the migrations.
