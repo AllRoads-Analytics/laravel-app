ab65d3654354dba6878c151c872ab11b22c47695
Tim <tim.cieplowski@gmail.com>
2022-01-06 09:51:58 -0500

    Add GA and AllRoads snippet to app. Update pathfinder to allroads in script copy/paste section.

--- app/Models/Tracker.php
+++ app/Models/Tracker.php
@@ -36,17 +36,17 @@ class Tracker extends ModelAbstract
     public function getCodeSnippet() {
         $code =
 <<<JS
-<!-- Start Pathfinder Snippet -->
+<!-- Start AllRoads Snippet -->
 <script>
 ! function(e, t, n, a, p, r, s) {
 e[a] || ((p = e[a] = function() {
 p.process ? p.process.apply(p, arguments) : p.queue.push(arguments)
-}).queue = [], p.t = +new Date, (r = t.createElement(n)).async = 1, r.src = "https://probable-skill-330219.ue.r.appspot.com/pathfinder.min.js?t=" + 864e5 * Math.ceil(new Date / 864e5), (s = t.getElementsByTagName(n)[0]).parentNode.insertBefore(r, s))
-}(window, document, "script", "pathfinder"),
-pathfinder("init", "$this->pixel_id", {follow: true}),
-pathfinder("event", "pageload");
+}).queue = [], p.t = +new Date, (r = t.createElement(n)).async = 1, r.src = "https://events.allroadsanalytics.com/allroads.min.js?t=" + 864e5 * Math.ceil(new Date / 864e5), (s = t.getElementsByTagName(n)[0]).parentNode.insertBefore(r, s))
+}(window, document, "script", "allroads"),
+allroads("init", "$this->pixel_id", {follow: true}),
+allroads("event", "pageload");
 </script>
-<!-- End Pathfinder Snippet -->
+<!-- End AllRoads Snippet -->
 JS;
 
         return $code;
