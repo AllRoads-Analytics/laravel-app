e9708eb2c9ab09ca14febf07c1b25f8edbde9238
Tim <tim.cieplowski@gmail.com>
2022-01-16 11:20:12 -0500

    update invite email

--- resources/views/mail/welcome.blade.php
+++ resources/views/mail/welcome.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Welcome to AllRoads Analytics
 
-Create or join an organization, install your tracking code, and start measuring all of the paths users take on your site(s)!
+Create or join a Tracker, install your tracking code, and start measuring all of the paths users take on your site(s)!
 
 @component('mail::button', [ 'url' => route('home') ])
 Get Started
