6e3934f207e77f3489eac3f52d87ba09aa12f583
Tim <tim.cieplowski@gmail.com>
2022-01-16 10:17:10 -0500

    update email label

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -2,7 +2,7 @@
 {{-- Header --}}
 @slot('header')
 @component('mail::header', ['url' => config('app.url')])
-{{ config('app.name') }}
+{{ config('app.label') }}
 @endcomponent
 @endslot
 
@@ -21,7 +21,7 @@
 {{-- Footer --}}
 @slot('footer')
 @component('mail::footer')
-© {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
+© {{ date('Y') }} {{ config('app.label') }}. @lang('All rights reserved.')
 @endcomponent
 @endslot
 @endcomponent
